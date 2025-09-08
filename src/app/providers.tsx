16b379497796701328c44ec4c6562adcb39374af
raykoshima <salonezamalone@gmail.com>
2025-09-08 17:57:57 +0700

    heroui provider

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+// app/providers.tsx
+'use client'
+
+import {HeroUIProvider} from '@heroui/react'
+import {ToastProvider} from "@heroui/toast";
+
+export function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <HeroUIProvider>
+        <ToastProvider />
+        {children}
+    </HeroUIProvider>
+  )
+}
